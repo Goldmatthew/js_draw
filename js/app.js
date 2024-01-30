d21771f4ae6a8c1be03c1921354a012e5ed49471
Mattia Doro <mattia.doro@gmail.com>
2024-01-30 17:09:34 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,46 +3,40 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const ctx = canvas.getContext('2d');
     let isDrawing = false;
 
-    // Imposta lo stile per il disegno
-    ctx.lineWidth = 2; // Larghezza della linea
-    ctx.lineJoin = 'round'; // Tipo di angolo quando due linee si incontrano
-    ctx.lineCap = 'round'; // Tipo di estremità della linea
-
-    // Funzione per aggiornare la larghezza della linea in base alla pressione
-    function setLineWidth(pressure) {
-        // Ad esempio, la larghezza della linea varia tra 0.5 e 5 in base alla pressione
-        ctx.lineWidth = pressure * 4.5 + 0.5;
-    }
-
     canvas.addEventListener('pointerdown', (e) => {
-        if (e.pointerType === 'pen') {
+        // Controlla se l'input è touch (oppure puoi rimuovere questa riga per gestire tutti i tipi di input)
+        if (e.pointerType === 'touch') {
             isDrawing = true;
             ctx.beginPath();
             ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
-            setLineWidth(e.pressure);
-            console.log(`Inizio a disegnare in posizione: ${e.clientX}, ${e.clientY} con pressione: ${e.pressure}`);
+            console.log(`Inizio a disegnare in posizione: ${e.clientX}, ${e.clientY}`);
         }
     });
 
     canvas.addEventListener('pointermove', (e) => {
-        if (e.pointerType === 'pen' && isDrawing) {
+        if (e.pointerType === 'touch' && isDrawing) {
             ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
-            setLineWidth(e.pressure);
             ctx.stroke();
-            console.log(`Mi sto muovendo in posizione: ${e.clientX}, ${e.clientY} con pressione: ${e.pressure}`);
+            console.log(`Mi sto muovendo in posizione: ${e.clientX}, ${e.clientY}`);
         }
     });
 
     canvas.addEventListener('pointerup', (e) => {
-        if (e.pointerType === 'pen') {
+        if (e.pointerType === 'touch') {
             isDrawing = false;
-            console.log(`Ho finito di disegnare in posizione: ${e.clientX}, ${e.clientY} con pressione: ${e.pressure}`);
+            console.log(`Ho finito di disegnare in posizione: ${e.clientX}, ${e.clientY}`);
         }
     });
 
     canvas.addEventListener('pointerleave', (e) => {
-        if (e.pointerType === 'pen') {
+        if (e.pointerType === 'touch') {
             isDrawing = false;
+        }
+    });
+
+    canvas.addEventListener('pointerenter', (e) => {
+        if (e.pointerType === 'touch' && isDrawing) {
+            ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
         }
     });
 
